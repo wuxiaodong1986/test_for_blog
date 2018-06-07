36b6143ea83293c30942aa5ae15622d8fa1b741f
wuxiaodong <308200039@qq.com>
2018-06-07 22:15:08 +0800

    add routing-datasource

--- src/main/java/com/test/config/DBContextHolder.java
+++ src/main/java/com/test/config/DBContextHolder.java
@@ -19,22 +19,14 @@ public class DBContextHolder {
     }
 
     /**
-     * 
      * 设置本线程的dbtype
-     * 
-     * @param str
-     * @see [相关类/方法](可选)
-     * @since [产品/模块版本](可选)
      */
     public static void setDbType(String str) {
         contextHolder.set(str);
     }
 
     /**
-     * clearDBType
-     * 
-     * @Title: clearDBType
-     * @Description: 清理连接类型
+     * 清理连接类型
      */
     public static void clearDBType() {
         contextHolder.remove();
