9b638f11b4e4acd8244caea34bb3e487d6bbe3a1
wuxiaodong <308200039@qq.com>
2018-08-06 17:56:58 +0800

    test

---
+++ src/main/java/com/test/service/StreamService.java
@@ -0,0 +1,18 @@
+package com.test.service;
+
+import org.assertj.core.util.Lists;
+
+import java.util.List;
+
+/**
+ * Created by 吴晓冬 on 2018/8/2.
+ */
+public class StreamService
+{
+    public static void main(String[] args)
+    {
+        List<Integer> list = Lists.newArrayList(3, 5, 1, 10, 8);
+
+        System.out.println(list);
+    }
+}
